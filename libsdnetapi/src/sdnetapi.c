6231df6f7f5f384eae65b3596f79d801eec3765b
Stacey Sheldon <stac@es.net>
2021-12-15 08:49:09 -0800

    sdnet: adapt to vivado 2021.2 renaming sdnet -> vitisnetp4

--- libsdnetapi/src/sdnetapi.c
+++ libsdnetapi/src/sdnetapi.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>		/* fprintf */
 #include "sdnetapi.h"		/* API */
-#include "sdnet_0_defs.h"	/* XilSdnetTargetConfig_sdnet_0 */
+#include "sdnet_0_defs.h"	/* XilVitisNetP4TargetConfig_sdnet_0 */
 #include "sdnetio.h"		/* sdnetio_reg_* */
 #include "unused.h"		/* UNUSED() */
 
@@ -15,13 +15,13 @@ struct sdnet_user_context {
   uintptr_t sdnet_base_addr;
 };
 
-static XilSdnetReturnType device_write(XilSdnetEnvIf *EnvIfPtr, XilSdnetAddressType address, uint32_t data) {
+static XilVitisNetP4ReturnType device_write(XilVitisNetP4EnvIf *EnvIfPtr, XilVitisNetP4AddressType address, uint32_t data) {
     // Validate inputs
     if (NULL == EnvIfPtr) {
-        return XIL_SDNET_GENERAL_ERR_NULL_PARAM;
+        return XIL_VITIS_NET_P4_GENERAL_ERR_NULL_PARAM;
     }
     if (NULL == EnvIfPtr->UserCtx)	{
-        return XIL_SDNET_GENERAL_ERR_INTERNAL_ASSERTION;
+        return XIL_VITIS_NET_P4_GENERAL_ERR_INTERNAL_ASSERTION;
     }
 
     // Recover virtual base address from user context
@@ -31,19 +31,19 @@ static XilSdnetReturnType device_write(XilSdnetEnvIf *EnvIfPtr, XilSdnetAddressT
     // Do the operation
     sdnetio_reg_write(user_ctx->sdnet_base_addr, address, data);
 
-    return XIL_SDNET_SUCCESS;
+    return XIL_VITIS_NET_P4_SUCCESS;
 }
 
-static XilSdnetReturnType device_read(XilSdnetEnvIf *EnvIfPtr, XilSdnetAddressType address, uint32_t *dataPtr) {
+static XilVitisNetP4ReturnType device_read(XilVitisNetP4EnvIf *EnvIfPtr, XilVitisNetP4AddressType address, uint32_t *dataPtr) {
     // Validate inputs
     if (EnvIfPtr == NULL) {
-        return XIL_SDNET_GENERAL_ERR_NULL_PARAM;
+        return XIL_VITIS_NET_P4_GENERAL_ERR_NULL_PARAM;
     }
     if (EnvIfPtr->UserCtx == NULL) {
-        return XIL_SDNET_GENERAL_ERR_INTERNAL_ASSERTION;
+        return XIL_VITIS_NET_P4_GENERAL_ERR_INTERNAL_ASSERTION;
     }
     if (dataPtr == NULL) {
-        return XIL_SDNET_GENERAL_ERR_INTERNAL_ASSERTION;
+        return XIL_VITIS_NET_P4_GENERAL_ERR_INTERNAL_ASSERTION;
     }
 
     // Recover virtual base address from user context
@@ -53,21 +53,21 @@ static XilSdnetReturnType device_read(XilSdnetEnvIf *EnvIfPtr, XilSdnetAddressTy
     // Do the operation
     sdnetio_reg_read(user_ctx->sdnet_base_addr, address, dataPtr);
 
-    return XIL_SDNET_SUCCESS;
+    return XIL_VITIS_NET_P4_SUCCESS;
 }
 
-static XilSdnetReturnType log_info(XilSdnetEnvIf *UNUSED(EnvIfPtr), const char *MessagePtr)
+static XilVitisNetP4ReturnType log_info(XilVitisNetP4EnvIf *UNUSED(EnvIfPtr), const char *MessagePtr)
 {
   fprintf(stdout, "%s\n", MessagePtr);
 
-  return XIL_SDNET_SUCCESS;
+  return XIL_VITIS_NET_P4_SUCCESS;
 }
 
-static XilSdnetReturnType log_error(XilSdnetEnvIf *UNUSED(EnvIfPtr), const char *MessagePtr)
+static XilVitisNetP4ReturnType log_error(XilVitisNetP4EnvIf *UNUSED(EnvIfPtr), const char *MessagePtr)
 {
   fprintf(stderr, "%s\n", MessagePtr);
 
-  return XIL_SDNET_SUCCESS;
+  return XIL_VITIS_NET_P4_SUCCESS;
 }
 
 void * sdnet_init(uintptr_t sdnet_base_addr)
@@ -79,30 +79,30 @@ void * sdnet_init(uintptr_t sdnet_base_addr)
   }
   sdnet_user->sdnet_base_addr = sdnet_base_addr;
 
-  XilSdnetEnvIf * sdnet_env;
-  sdnet_env = (XilSdnetEnvIf *) calloc(1, sizeof(XilSdnetEnvIf));
+  XilVitisNetP4EnvIf * sdnet_env;
+  sdnet_env = (XilVitisNetP4EnvIf *) calloc(1, sizeof(XilVitisNetP4EnvIf));
   if (sdnet_env == NULL) {
     goto out_fail_user;
   }
 
-  XilSdnetTargetCtx * sdnet_target;
-  sdnet_target = (XilSdnetTargetCtx *) calloc(1, sizeof(XilSdnetEnvIf));
+  XilVitisNetP4TargetCtx * sdnet_target;
+  sdnet_target = (XilVitisNetP4TargetCtx *) calloc(1, sizeof(XilVitisNetP4EnvIf));
   if (sdnet_target == NULL) {
     goto out_fail_env;
   }
 
   // Initialize the sdnet env
-  if (XilSdnetStubEnvIf(sdnet_env) != XIL_SDNET_SUCCESS) {
+  if (XilVitisNetP4StubEnvIf(sdnet_env) != XIL_VITIS_NET_P4_SUCCESS) {
     goto out_fail_target;
   }
-  sdnet_env->WordWrite32 = (XilSdnetWordWrite32Fp) &device_write;
-  sdnet_env->WordRead32  = (XilSdnetWordRead32Fp)  &device_read;
-  sdnet_env->UserCtx     = (XilSdnetUserCtxType)   sdnet_user;
-  sdnet_env->LogError    = (XilSdnetLogFp)         &log_error;
-  sdnet_env->LogInfo     = (XilSdnetLogFp)         &log_info;
+  sdnet_env->WordWrite32 = (XilVitisNetP4WordWrite32Fp) &device_write;
+  sdnet_env->WordRead32  = (XilVitisNetP4WordRead32Fp)  &device_read;
+  sdnet_env->UserCtx     = (XilVitisNetP4UserCtxType)   sdnet_user;
+  sdnet_env->LogError    = (XilVitisNetP4LogFp)         &log_error;
+  sdnet_env->LogInfo     = (XilVitisNetP4LogFp)         &log_info;
 
   // Initialize the sdnet target
-  if (XilSdnetTargetInit(sdnet_target, sdnet_env, &XilSdnetTargetConfig_sdnet_0) != XIL_SDNET_SUCCESS) {
+  if (XilVitisNetP4TargetInit(sdnet_target, sdnet_env, &XilVitisNetP4TargetConfig_sdnet_0) != XIL_VITIS_NET_P4_SUCCESS) {
     goto out_fail_target;
   }
 
@@ -120,9 +120,9 @@ void * sdnet_init(uintptr_t sdnet_base_addr)
 
 bool sdnet_deinit(void * sdnet_handle)
 {
-  XilSdnetTargetCtx * sdnet_target = (XilSdnetTargetCtx *) sdnet_handle;
+  XilVitisNetP4TargetCtx * sdnet_target = (XilVitisNetP4TargetCtx *) sdnet_handle;
 
-  if (XilSdnetTargetExit(sdnet_target) != XIL_SDNET_SUCCESS) {
+  if (XilVitisNetP4TargetExit(sdnet_target) != XIL_VITIS_NET_P4_SUCCESS) {
     return false;
   }
 
@@ -131,21 +131,21 @@ bool sdnet_deinit(void * sdnet_handle)
 
 bool sdnet_reset_all_tables(void * sdnet_handle)
 {
-  XilSdnetTargetCtx * sdnet_target = (XilSdnetTargetCtx *) sdnet_handle;
+  XilVitisNetP4TargetCtx * sdnet_target = (XilVitisNetP4TargetCtx *) sdnet_handle;
 
   // Look up the number of tables in the design
   uint32_t num_tables;
-  if (XilSdnetTargetGetTableCount(sdnet_target, &num_tables) != XIL_SDNET_SUCCESS) {
+  if (XilVitisNetP4TargetGetTableCount(sdnet_target, &num_tables) != XIL_VITIS_NET_P4_SUCCESS) {
     return false;
   }
 
   // Reset all of the tables in the design
   for (uint32_t i = 0; i < num_tables; i++) {
-    XilSdnetTableCtx * table;
-    if (XilSdnetTargetGetTableByIndex(sdnet_target, i, &table) != XIL_SDNET_SUCCESS) {
+    XilVitisNetP4TableCtx * table;
+    if (XilVitisNetP4TargetGetTableByIndex(sdnet_target, i, &table) != XIL_VITIS_NET_P4_SUCCESS) {
       return false;
     }
-    if (XilSdnetTableReset(table) != XIL_SDNET_SUCCESS) {
+    if (XilVitisNetP4TableReset(table) != XIL_VITIS_NET_P4_SUCCESS) {
       return false;
     }
   }
@@ -164,30 +164,30 @@ bool sdnet_table_insert_kma(void * sdnet_handle,
 			    size_t UNUSED(params_len),
 			    uint32_t priority)
 {
-  XilSdnetTargetCtx * sdnet_target = (XilSdnetTargetCtx *) sdnet_handle;
+  XilVitisNetP4TargetCtx * sdnet_target = (XilVitisNetP4TargetCtx *) sdnet_handle;
 
   // Get a handle for the target table
-  XilSdnetTableCtx * table;
-  if (XilSdnetTargetGetTableByName(sdnet_target, (char *)table_name, &table) != XIL_SDNET_SUCCESS) {
+  XilVitisNetP4TableCtx * table;
+  if (XilVitisNetP4TargetGetTableByName(sdnet_target, (char *)table_name, &table) != XIL_VITIS_NET_P4_SUCCESS) {
     return false;
   }
 
   // Convert the action name to an id
   uint32_t action_id;
-  if (XilSdnetTableGetActionId(table, (char *)action_name, &action_id) != XIL_SDNET_SUCCESS) {
+  if (XilVitisNetP4TableGetActionId(table, (char *)action_name, &action_id) != XIL_VITIS_NET_P4_SUCCESS) {
     return false;
   }
 
-  XilSdnetTableMode table_mode;
-  if (XilSdnetTableGetMode(table, &table_mode) != XIL_SDNET_SUCCESS) {
+  XilVitisNetP4TableMode table_mode;
+  if (XilVitisNetP4TableGetMode(table, &table_mode) != XIL_VITIS_NET_P4_SUCCESS) {
     return false;
   }
 
   // Certain table modes insist on a NULL mask parameter
   switch (table_mode) {
-  case XIL_SDNET_TABLE_MODE_DCAM:
-  case XIL_SDNET_TABLE_MODE_BCAM:
-  case XIL_SDNET_TABLE_MODE_TINY_BCAM:
+  case XIL_VITIS_NET_P4_TABLE_MODE_DCAM:
+  case XIL_VITIS_NET_P4_TABLE_MODE_BCAM:
+  case XIL_VITIS_NET_P4_TABLE_MODE_TINY_BCAM:
     // Mask parameter must be NULL for these table modes
     mask = NULL;
     break;
@@ -196,7 +196,7 @@ bool sdnet_table_insert_kma(void * sdnet_handle,
     break;
   }
 
-  if (XilSdnetTableInsert(table, key, mask, priority, action_id, params) != XIL_SDNET_SUCCESS) {
+  if (XilVitisNetP4TableInsert(table, key, mask, priority, action_id, params) != XIL_VITIS_NET_P4_SUCCESS) {
     return false;
   }
 
@@ -210,24 +210,24 @@ bool sdnet_table_delete_k(void * sdnet_handle,
 			  uint8_t * mask,
 			  size_t    UNUSED(mask_len))
 {
-  XilSdnetTargetCtx * sdnet_target = (XilSdnetTargetCtx *) sdnet_handle;
+  XilVitisNetP4TargetCtx * sdnet_target = (XilVitisNetP4TargetCtx *) sdnet_handle;
 
   // Get a handle for the target table
-  XilSdnetTableCtx * table;
-  if (XilSdnetTargetGetTableByName(sdnet_target, (char *)table_name, &table) != XIL_SDNET_SUCCESS) {
+  XilVitisNetP4TableCtx * table;
+  if (XilVitisNetP4TargetGetTableByName(sdnet_target, (char *)table_name, &table) != XIL_VITIS_NET_P4_SUCCESS) {
     return false;
   }
 
-  XilSdnetTableMode table_mode;
-  if (XilSdnetTableGetMode(table, &table_mode) != XIL_SDNET_SUCCESS) {
+  XilVitisNetP4TableMode table_mode;
+  if (XilVitisNetP4TableGetMode(table, &table_mode) != XIL_VITIS_NET_P4_SUCCESS) {
     return false;
   }
 
   // Certain table modes insist on a NULL mask parameter
   switch (table_mode) {
-  case XIL_SDNET_TABLE_MODE_DCAM:
-  case XIL_SDNET_TABLE_MODE_BCAM:
-  case XIL_SDNET_TABLE_MODE_TINY_BCAM:
+  case XIL_VITIS_NET_P4_TABLE_MODE_DCAM:
+  case XIL_VITIS_NET_P4_TABLE_MODE_BCAM:
+  case XIL_VITIS_NET_P4_TABLE_MODE_TINY_BCAM:
     // Mask parameter must be NULL for these table modes
     mask = NULL;
     break;
@@ -236,7 +236,7 @@ bool sdnet_table_delete_k(void * sdnet_handle,
     break;
   }
 
-  if (XilSdnetTableDelete(table, key, mask) != XIL_SDNET_SUCCESS) {
+  if (XilVitisNetP4TableDelete(table, key, mask) != XIL_VITIS_NET_P4_SUCCESS) {
     return false;
   }
 
