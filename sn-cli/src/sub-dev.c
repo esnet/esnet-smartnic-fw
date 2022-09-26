e1be144990c2e91b1877ee8e03b0b52901c3e781
Stacey Sheldon <stac@es.net>
2022-09-26 11:05:48 -0700

    sn-cli: adapt to regif -> smartnic_322mhz_regs block renaming

--- sn-cli/src/sub-dev.c
+++ sn-cli/src/sub-dev.c
@@ -132,7 +132,7 @@ void cmd_dev(struct argp_state *state)
 
   if (!strcmp(arguments.command, "version")) {
     printf("Device Version Info\n");
-    printf("\tUSR_ACCESS:    0x%08x (%u)\n", bar2->regif.usr_access, bar2->regif.usr_access);
+    printf("\tUSR_ACCESS:    0x%08x (%u)\n", bar2->smartnic_322mhz_regs.usr_access, bar2->smartnic_322mhz_regs.usr_access);
     printf("\tBUILD_STATUS:  0x%08x\n", bar2->syscfg.build_status);
   } else if (!strcmp(arguments.command, "temp")) {
     printf("Temperature Monitors\n");
