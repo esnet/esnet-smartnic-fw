e790d74332f19a7f2dcff7ee7cf5a335470dc1cf
Stacey Sheldon <stac@es.net>
2024-10-28 23:05:19 +0000

    stack: smartnic-lldp: allow override of system, component and sw version

--- sn-stack/smartnic-lldp/scripts/sn_lldp.py
+++ sn-stack/smartnic-lldp/scripts/sn_lldp.py
@@ -22,18 +22,34 @@ load_contrib("lldp")
               help="Hostname FQDN (eg. lbnl59-ht1.es.net)",
               required=True,
 )
+@click.option('--sys-desc',
+              help="LLDP System Description prefix to send",
+              default="ESnet SmartNIC",
+              show_default=True,
+)
+@click.option('--component',
+              help="LLDP Chassis Component prefix to send",
+              default="ESnet SmartNIC",
+              show_default=True,
+)
+@click.option('--sw-ver',
+              help="Software version to include in LLDP System Description",
+)
 @click.option('-t', '--ttl',
               help="LLDP Time To Live in seconds.  Note that msgs are sent every TTL/2.",
               default=20,
               show_default=True,
 )
 
-def main(slotaddr, host, ttl):
+def main(slotaddr, host, ttl, sys_desc, component, sw_ver):
 
     print("LLDP Configuration")
-    print("  PCIe Slot: {:s}".format(slotaddr))
-    print("  Host FQDN: {:s}".format(host))
-    print("  TTL      : {:d} seconds".format(ttl))
+    print("  PCIe Slot : {:s}".format(slotaddr))
+    print("  Host FQDN : {:s}".format(host))
+    print("  Component : {:s}".format(component))
+    print("  Sys Desc  : {:s}".format(sys_desc))
+    print("  SW Version: {:s}".format(sw_ver if sw_ver else "<not provided>"))
+    print("  TTL       : {:d} seconds".format(ttl))
 
     # Build up the LLDP Context objects and generate the LLDP packets
     LLDPContext = namedtuple('LLDPContext', [
@@ -49,19 +65,29 @@ def main(slotaddr, host, ttl):
     lldp_contexts = []
     for tapif, portid in port_bindings:
         print("  {} -> {}".format(tapif, portid))
+        component_full = "{:s} {:s}".format(
+            component,
+            slotaddr
+        )
+        sys_desc_full   = "{:s} HW={:s} FW={:s}".format(
+            sys_desc,
+            os.environ['SN_HW_VER'],
+            os.environ['SN_FW_VER'],
+        )
+        if sw_ver is not None:
+            sys_desc_full += " SW={:s}".format(sw_ver)
+
         packet = (
             Ether(dst="01:80:c2:00:00:0e",
                   src=get_if_hwaddr(tapif)) /
             LLDPDU() /
             LLDPDUChassisID(subtype="chassis component",
-                            id="ESnet SmartNIC " + slotaddr) /
+                            id=component_full) /
             LLDPDUPortID(subtype="interface name",
                          id=portid) /
             LLDPDUTimeToLive(ttl=ttl) /
             LLDPDUSystemName(system_name=host) /
-            LLDPDUSystemDescription(description="ESnet SmartNIC HW=" +
-                                    os.environ['SN_HW_VER'] +
-                                    " FW=" + os.environ['SN_FW_VER']) /
+            LLDPDUSystemDescription(description = sys_desc_full) /
             LLDPDUEndOfLLDPDU()
         )
         lldp_contexts.append(LLDPContext(tapif=tapif, packet=packet))
