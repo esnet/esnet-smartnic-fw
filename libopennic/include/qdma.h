39d3985d498907ad36d96b819731a48c959548df
Jeremy Ranger <jranger@es.net>
2024-12-12 10:31:20 -0500

    libopennic: Add support for per-function QDMA queues

--- libopennic/include/qdma.h
+++ libopennic/include/qdma.h
@@ -1,8 +1,7 @@
 #ifndef INCLUDE_QDMA_H
 #define INCLUDE_QDMA_H
 
-#include "cmac_adapter_block.h"
-#include "qdma_function_block.h"
+#include "smartnic.h"
 #include "stats.h"
 #include <stdbool.h>
 
@@ -11,10 +10,25 @@ extern "C" {
 #endif
 
 //--------------------------------------------------------------------------------------------------
-bool qdma_get_queues(volatile struct qdma_function_block* qdma,
-                     unsigned int* base_queue, unsigned int* num_queues);
-bool qdma_set_queues(volatile struct qdma_function_block* qdma,
-                     unsigned int base_queue, unsigned int num_queues);
+#define QDMA_CHANNEL_MAX 2
+enum qdma_function {
+    qdma_function_MIN,
+    qdma_function_PF = qdma_function_MIN,
+
+    qdma_function_VF_MIN,
+    qdma_function_VF0 = qdma_function_VF_MIN,
+    qdma_function_VF1,
+    qdma_function_VF2,
+    qdma_function_VF_MAX,
+
+    qdma_function_MAX = qdma_function_VF_MAX
+};
+bool qdma_function_get_queues(volatile struct esnet_smartnic_bar2* bar2,
+                              unsigned int channel, enum qdma_function func,
+                              unsigned int* base_queue, unsigned int* num_queues);
+bool qdma_function_set_queues(volatile struct esnet_smartnic_bar2* bar2,
+                              unsigned int channel, enum qdma_function func,
+                              unsigned int base_queue, unsigned int num_queues);
 
 struct stats_zone* qdma_stats_zone_alloc(struct stats_domain* domain,
                                          volatile struct cmac_adapter_block* adapter,
